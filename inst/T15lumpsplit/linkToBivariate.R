598d00893a0a010d8671367f031648f9204361bf
Roger Day <notsofeo@gmail.com>
2018-05-04 15:28:27 -0400

    A solution to link-outs.

--- inst/T15lumpsplit/linkToBivariate.R
+++ inst/T15lumpsplit/linkToBivariate.R
@@ -1,11 +1,22 @@
 
 #```{r}
+
+#This works
+interactiveSection(codeToRun = "system('open ./lump,split-bivariate-normal-derivation.pdf')")
+# Even better:
+theFile = system.file(
+  package="T15lumpsplit",
+  'T15lumpsplit/lump,split-bivariate-normal-derivation.pdf')
+interactiveSection(codeToRun = paste0(
+  "system('open ", theFile, "')") )
+
+
+
+## These do not.
 div(HTML("<a
          href='http://localhost/~Roger/lump,split-bivariate-normal-derivation.pdf' target='_blank' > Click here to view the derivation of the posterior in another window.</a>")
 )
-div(HTML("<a
-         href='http://localhost/~Roger/lump,split-bivariate-normal-derivation.pdf' target='_blank' > Click here to view the derivation of the posterior in another window.</a>")
-)
+
 div(HTML("<a
          href='www/lump,split-bivariate-normal-derivation.pdf' target='_blank' rel='noopener'> Click here to view the derivation of the posterior in another window.</a>")
 )     ####doesn't work
