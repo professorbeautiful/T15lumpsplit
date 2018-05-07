bfb5e2836408385e7d20e2199cf5cb6e9c584aa8
Roger Day <notsofeo@gmail.com>
2018-05-07 00:23:42 -0400

    We don't need a stylesheet in inclRmd

--- inst/T15lumpsplit/inclRmd.R
+++ inst/T15lumpsplit/inclRmd.R
@@ -4,8 +4,9 @@ inclRmd <- function(path) {
     return(paste("inclRmd: file ", path, " not found"))
   paste(readLines(path, warn = FALSE), collapse = '\n') %>%
     knitr::knit2html(quiet=TRUE,
-                     text = ., fragment.only = TRUE, options = "",
-                     stylesheet=file.path(r_path,"../www/empty.css")
+                     text = ., fragment.only = TRUE, options = ""
+                     #,stylesheet=file.path(r_path,"../www/empty.css"
+
     # rmarkdown::render(quiet=TRUE,output_format = 'html_document',
                       # input = path, runtime='shiny'
     ) %>%
