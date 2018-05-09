3d9edf528b9d4ace4c2cdb5fb2a60fabff0a3c51
Roger Day <notsofeo@gmail.com>
2018-05-08 23:33:31 -0400

    Beginning to set the stage for Dr. Who in the plot.

--- inst/T15lumpsplit/Plight-Pdark-posterior-new.R
+++ inst/T15lumpsplit/Plight-Pdark-posterior-new.R
@@ -2,6 +2,7 @@ plotPlightPdarkPosterior = function(
   DLdata = matrix(c(3,5,2,90),nrow=2),
   tau, phi, mu0,
   showPrior = TRUE, showPosterior=TRUE, showLikelihood=TRUE,
+  showSandLandWho = FALSE,
   fudgeFactor = 0.001,
   addFudge = TRUE
 ) {
@@ -10,25 +11,25 @@ plotPlightPdarkPosterior = function(
   varhat = apply(DLdata, 1, function(r)sum(1/r))
   if(any(DLdata==0)) fudgeFactor = max(fudgeFactor, 0.001)
     # if wanted, or if needed (if any zero cells).
-  DLdataFudged = DLdata + fudgeFactor  
+  DLdataFudged = DLdata + fudgeFactor
   logit.hat.fudged = logit(apply(DLdataFudged, 1, function(r)r[1]/sum(r)))
   varhat.fudged = apply(DLdataFudged, 1, function(r)sum(1/r))
   ### deltat method with protection from zero's.
-  sig11 = sig12 = sig21 = matrix(c(tau+phi,tau,tau,tau+phi),nrow=2)  
+  sig11 = sig12 = sig21 = matrix(c(tau+phi,tau,tau,tau+phi),nrow=2)
   sig22 = sig11 + diag(varhat)   ## marginal variance of the data?
   logit.prior.mean = c(mu0, mu0)
   ## always use fudged here:
-  postmean.logit = logit.prior.mean + 
-    sig12%*%solve(sig22) %*% (logit.hat.fudged-logit.prior.mean) 
+  postmean.logit = logit.prior.mean +
+    sig12%*%solve(sig22) %*% (logit.hat.fudged-logit.prior.mean)
   postmean.p = antilogit(postmean.logit)
   postvar.logit = sig11 - sig12%*%solve(sig22)%*%sig21
   ####################
-  
+
   svdSig11 = svd(sig11)
   sqrtSig11 = svdSig11$v %*% diag(sqrt(svdSig11$d)) %*% svdSig11$u
   svdPostvar.logit = svd(postvar.logit)
   sqrtPostvar.logit = svdPostvar.logit$v %*% diag(sqrt(svdPostvar.logit$d)) %*% svdPostvar.logit$u
-  
+
   angles = seq(0,2*pi,length=100)
   qlevel = qchisq(p=0.99, df=2)
   circlepoints = cbind(cos(angles),sin(angles))
@@ -37,11 +38,11 @@ plotPlightPdarkPosterior = function(
   if(showPrior) {
     for (plevel in seq(.1,.9,.1)) {
       qlevel = qchisq(p=plevel, df=2)
-      contourlevel1 = antilogit(logit.prior.mean 
+      contourlevel1 = antilogit(logit.prior.mean
                                 + (circlepoints*qlevel) %*% sqrtSig11)
       lines(contourlevel1, col=ColorForPrior)
       if(plevel==.5){
-        contourlevel1 = antilogit( logit.prior.mean 
+        contourlevel1 = antilogit( logit.prior.mean
                                    + (cbind(cos(angles),sin(angles)) * qlevel ) %*% sqrtSig11)
         xorder = order(contourlevel1[ ,1])
         lines(contourlevel1[xorder,1], contourlevel1[xorder,2],col = ColorForPrior, lty=2)
@@ -67,11 +68,11 @@ plotPlightPdarkPosterior = function(
 #  if(showLikelihood) {
 #    argmin = function(v, target=0) which(abs(v-target) == min(abs(v-target))[1])
 #     probs = seq(0.01, 0.99, by = 0.01)
-#     normalizedLikelihoodDark = dbinom(x = DLdata["dark", "R"], size=sum(DLdata["dark", ]), 
+#     normalizedLikelihoodDark = dbinom(x = DLdata["dark", "R"], size=sum(DLdata["dark", ]),
 #                                       prob=probs)
 #     normalizedLikelihoodDark = normalizedLikelihoodDark/sum(normalizedLikelihoodDark)/0.01
 #     cumLikelihoodDark = cumsum(normalizedLikelihoodDark)
-#     normalizedLikelihoodLight = dbinom(x = DLdata["light", "R"], size=sum(DLdata["light", ]), 
+#     normalizedLikelihoodLight = dbinom(x = DLdata["light", "R"], size=sum(DLdata["light", ]),
 #                                       prob=probs)
 #     normalizedLikelihoodLight = normalizedLikelihoodLight/sum(normalizedLikelihoodLight)/0.01
 #     cumLikelihoodLight = cumsum(normalizedLikelihoodLight)
@@ -84,7 +85,7 @@ plotPlightPdarkPosterior = function(
 #       lines(c(D1, D1, D2, D2), c(L1, L2, L2, L1), col= ColorForLikelihood)
 #      }
 #  }
-  points(antilogit(logit.hat)[1], antilogit(logit.hat)[2],	
+  points(antilogit(logit.hat)[1], antilogit(logit.hat)[2],
          pch = "X", cex=3)
   mtext(text = paste("posterior mean for Pr(R | D) = ",
                      round(digits=4, postmean.p[1])),
