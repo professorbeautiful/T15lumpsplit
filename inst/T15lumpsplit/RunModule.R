536b4aaebc9324ac54d0f95c9fd9425e636a57b0
Roger Day <notsofeo@gmail.com>
2018-05-17 15:02:42 -0400

    More failed attempts to automate knitting.

--- inst/T15lumpsplit/RunModule.R
+++ inst/T15lumpsplit/RunModule.R
@@ -1,7 +1,15 @@
+options(servr.daemon = TRUE)
+#### Still not working--- the shiny and the TeX don't process.
+
 homeDir = getwd()
 setwd('inst/T15lumpsplit')
-knitr::knit(input = 'Bias,variance,smoothing,shrinking.Rmd',
-            output = 'Bias,variance,smoothing,shrinking.html')
+# knitr::knit(input = 'Bias,variance,smoothing,shrinking.Rmd',
+#             output = 'Bias,variance,smoothing,shrinking.html')
+rmarkdown::render(input = 'Bias,variance,smoothing,shrinking.Rmd',
+            #output_format='all',
+            output_file = 'Bias,variance,smoothing,shrinking.html',
+            runtime='shiny',
+            clean=FALSE, quiet=FALSE)
 
 browseURL('Bias,variance,smoothing,shrinking.html',
           browser = '/Applications/Google\\ Chrome.app/Contents/MacOS/Google\\ Chrome')
