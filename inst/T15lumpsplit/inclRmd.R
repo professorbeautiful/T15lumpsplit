0b25e53f8106acf82d45dc8b0cb41020473dd18d
Roger Day <notsofeo@gmail.com>
2018-05-27 22:32:15 -0500

    inclRmd saves results in a file for inspection and troubleshooting (for example..why no subtitle numbering?)

--- inst/T15lumpsplit/inclRmd.R
+++ inst/T15lumpsplit/inclRmd.R
@@ -1,8 +1,12 @@
 require("magrittr")
 inclRmd <- function(path) {
+  tee = function(x, outputpath) {
+    writeLines(capture.output(x), con=outputpath)
+    return(x)
+  }
   if(!file.exists(path))
     return(paste("inclRmd: file ", path, " not found"))
-  paste(readLines(path, warn = FALSE), collapse = '\n') %>%
+  knitrOutput = paste(readLines(path, warn = FALSE), collapse = '\n') %>%
     knitr::knit2html(quiet=TRUE,
                      text = ., fragment.only = TRUE, options = ""
                      #,stylesheet=file.path(r_path,"../www/empty.css"
@@ -13,5 +17,8 @@ inclRmd <- function(path) {
     gsub("&lt;!--/html_preserve--&gt;","",.) %>%
     gsub("&lt;!--html_preserve--&gt;","",.) %>%
     HTML %>%
-    withMathJax
+    withMathJax %>%
+  #print(str(knitrOutput))
+  #return(knitrOutput)
+  tee(., paste0(path, '.inclOutput.Rmd'))
 }
