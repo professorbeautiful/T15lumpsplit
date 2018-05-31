b04cf3add535cd6dc3dd58364fdd70fc1fc83bad
Roger Day <notsofeo@gmail.com>
2018-05-30 23:45:38 -0500

    Slight tweak of QandAha. Still can't make the label wider.

--- inst/T15lumpsplit/QandAha.R
+++ inst/T15lumpsplit/QandAha.R
@@ -2,11 +2,11 @@ QandAha = function(buttonLabel= "save") {
   thisQANumber = nextNumber(sequenceType = "QA")
   outputIdThisQA = paste0('QA', thisQANumber)
   textareaIdThisQA = paste0('id', outputIdThisQA)
-  output[[outputIdThisQA]] = renderUI({
-    textAreaInput(inputId = textareaIdThisQA, width='100%',
-                  label = paste("(", thisQANumber, ")",
+  # output[[outputIdThisQA]] = renderUI({
+    textAreaInput(inputId = textareaIdThisQA, width='200%',
+                  label = paste("(QA:", thisQANumber, ")",
                                 "Ask a question and/or describe an 'aha' here.")
     )
-  })
-  uiOutput(outputId = outputIdThisQA)
+  # })
+  # uiOutput(outputId = outputIdThisQA)
 }
