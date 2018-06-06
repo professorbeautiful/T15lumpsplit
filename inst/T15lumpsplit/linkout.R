c49f285cd5893b4baf0b3ffee6d1080633beb846
Roger Day <notsofeo@gmail.com>
2018-06-06 14:15:22 -0400

    Add linkin and fix linkinLink and linkoutLink

--- inst/T15lumpsplit/linkout.R
+++ inst/T15lumpsplit/linkout.R
@@ -13,8 +13,8 @@ linkout = function(fileName) {
 
 linkoutLink = function(fileName, linkouttext) {
   # For opening a file in the package
-  thisWhoPriorProbNumber = nextNumber(sequenceType = "WhoPriorProb")
-  IdThisLinkout = paste0('linkoutLink', thisWhoPriorProbNumber)
+  thisNumber = nextNumber(sequenceType = "linkoutLink")
+  IdThisLinkout = paste0('linkoutLink', thisNumber)
   observeEvent(input[[IdThisLinkout]], linkout(fileName)
   )
   actionLink(IdThisLinkout,
@@ -24,19 +24,43 @@ linkoutLink = function(fileName, linkouttext) {
 }
 
 ## This doesn't work yet.
+## This address works: http://127.0.0.1:7767/Bias,variance,smoothing,shrinking.Rmd#section-a_crossvalidationPlot
+##  ( in address bar)
 linkinLink = function(anchorName, linktext) {
   # For going to a local anchor
-  thisWhoPriorProbNumber = nextNumber(sequenceType = "WhoPriorProb")
-  IdThisLinkout = paste0('linkoutLink', thisWhoPriorProbNumber)
-  observeEvent(input[[IdThisLinkout]], {
-               theCommand = paste0(
-                 "browseURL('#", anchorName, "')")
-               eval(parse(text=theCommand))
+  thisNumber = nextNumber(sequenceType = "linkinLink")
+  IdThisLinkin = paste0('linkinLink', thisNumber)
+  thisAnchorName <<- anchorName
+  observeEvent(input[[IdThisLinkin]], {
+    print(anchorName)
+    print(force(anchorName))
+    print(thisAnchorName)
+    linkin(force(thisAnchorName))
   }
   )
-  actionLink(IdThisLinkout,
+  actionLink(IdThisLinkin,
              label=HTML(
                paste0('<font color=blue>', linktext, '</font>'))
   )
 }
+
+linkin = function(anchorName) {
+  port = rValues$thisSession$clientData$url_port
+  print(paste("port=", port))
+  address = paste0(
+    'http://127.0.0.1:', port,
+    '/Bias,variance,smoothing,shrinking.Rmd#section-',
+    anchorName)
+  theCommand = paste0(
+    'browseURL("', address, '")')
+  print(paste('theCommand=', theCommand))
+  eval(parse(text=theCommand))
+}
+#From session help:
+#url_protocol, url_hostname, url_port, url_pathname, url_search,
+#url_hash_initial and url_hash can be used to get the components of the URL that
+#was requested by the browser to load the Shiny app page. These values are from
+#the browser's perspective, so neither HTTP proxies nor Shiny Server will affect
+#these values. The url_search value may be used with parseQueryString to access
+#query string parameters.
 
