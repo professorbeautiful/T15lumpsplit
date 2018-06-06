0415808c422ac19f3842b07a3d1e90d7f58fad5a
Roger Day <notsofeo@gmail.com>
2018-06-05 21:58:40 -0400

    Adding linkoutLink() and linkinLink(). An advantage of linkinLink--if it worked:  capturing the current location for return.

--- inst/T15lumpsplit/linkout.R
+++ inst/T15lumpsplit/linkout.R
@@ -7,3 +7,36 @@ linkout = function(fileName) {
   eval(parse(text=theCommand))
 #  "system('open ", theFile, "')")
 }
+
+# NOTE: for local anchors, the name is prepended with 'section-'
+# at the target, but not at the clickbait.
+
+linkoutLink = function(fileName, linkouttext) {
+  # For opening a file in the package
+  thisWhoPriorProbNumber = nextNumber(sequenceType = "WhoPriorProb")
+  IdThisLinkout = paste0('linkoutLink', thisWhoPriorProbNumber)
+  observeEvent(input[[IdThisLinkout]], linkout(fileName)
+  )
+  actionLink(IdThisLinkout,
+             label=HTML(
+               paste0('<font color=blue>', linkouttext, '</font>'))
+  )
+}
+
+## This doesn't work yet.
+linkinLink = function(anchorName, linktext) {
+  # For going to a local anchor
+  thisWhoPriorProbNumber = nextNumber(sequenceType = "WhoPriorProb")
+  IdThisLinkout = paste0('linkoutLink', thisWhoPriorProbNumber)
+  observeEvent(input[[IdThisLinkout]], {
+               theCommand = paste0(
+                 "browseURL('#", anchorName, "')")
+               eval(parse(text=theCommand))
+  }
+  )
+  actionLink(IdThisLinkout,
+             label=HTML(
+               paste0('<font color=blue>', linktext, '</font>'))
+  )
+}
+
