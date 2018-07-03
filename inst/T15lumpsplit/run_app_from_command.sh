3b0369e5619e7b90c82813349446a1c98e9ca1ba
Roger Day <notsofeo@gmail.com>
2018-07-03 13:49:25 -0400

    run the app from a command, not yet working.

---
+++ inst/T15lumpsplit/run_app_from_command.sh
@@ -0,0 +1,4 @@
+## Attempt to run the app from a command.
+## Not successful so far.
+
+/usr/local/bin/pandoc +RTS -K512m -RTS /private/var/folders/6v/y_cp6z4d22ggbgrqdk0g73v80000gv/T/RtmpOJU6le/Bias,variance,smoothing,shrinking.utf8.md --to html4 --from markdown+autolink_bare_uris+ascii_identifiers+tex_math_single_backslash+smart --output /private/var/folders/6v/y_cp6z4d22ggbgrqdk0g73v80000gv/T/RtmpOJU6le/file1409f7c6c4fc4.html --email-obfuscation none --standalone --section-divs --table-of-contents --toc-depth 6 --variable toc_float=1 --variable toc_selectors=h1,h2,h3,h4,h5,h6 --variable toc_collapsed=1 --variable toc_print=1 --template /Library/Frameworks/R.framework/Versions/3.5/Resources/library/rmarkdown/rmd/h/default.html --no-highlight --variable highlightjs=1 --number-sections --css lumpsplit.css --id-prefix section- --variable 'theme:bootstrap' --mathjax --variable 'mathjax-url:https://mathjax.rstudio.com/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML
