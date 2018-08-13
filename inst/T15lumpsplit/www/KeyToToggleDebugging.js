85ee15d3ac7b33cd7bfb0e0c90fc01a6df9a723f
Roger Day <notsofeo@gmail.com>
2018-08-13 11:13:31 -0400

    ctrlDpressed as a toggle; and transfer it to shiny's input object.

--- inst/T15lumpsplit/www/KeyToToggleDebugging.js
+++ inst/T15lumpsplit/www/KeyToToggleDebugging.js
@@ -3,8 +3,13 @@ var ctrlDpressed = false;
 $(document).on("keyup", function (e) {
     var x = event.which || event.keyCode;
     if(x==68 && e.ctrlKey) { // control d
-      ctrlDpressed = true;
+      ctrlDpressed = ! ctrlDpressed;  // toggle
+      Shiny.onInputChange("ctrlDpressed", ctrlDpressed);
+
       //navigateToId(currentLocationId);
-      alert('control d ' + e.ctrlKey + ' ' + event.ctrlKey);
+      //alert('control d ' + e.ctrlKey + ' ' + event.ctrlKey);
+      // The alert works.
+      // The panel appears only after something else JS happens.
+      //
     }
 });
