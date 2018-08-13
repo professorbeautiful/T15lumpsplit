f2e24f6cfe4de6ed122e62663f9f414c6fa11002
Roger Day <notsofeo@gmail.com>
2018-08-13 11:51:02 -0400

    move account info into misc. we don't want it on github!

--- R/deploy.R
+++ R/deploy.R
@@ -1,4 +1,3 @@
-# shinyapps::setAccountInfo(name='trials', token='0B4BEE2FB18CEA0BCFD76843D64453A5', secret='z30BF4mYXM3llWXFsbjF7I826F8DtVqaXUN3UyjY')
 
 # NOTE: I had tried to run rstan, and followed directions
 # for soft links as in "NOTES on rstan for monte carlo markov chain.rtf"
