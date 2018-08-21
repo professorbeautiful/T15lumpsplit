e13013f0264eb39e9cee42d10b17f95e56642a01
Roger Day <notsofeo@gmail.com>
2018-08-21 19:57:05 -0400

    display:block; width: 200% allows the QA lable to extend and not wrap.

--- inst/T15lumpsplit/QandAha.R
+++ inst/T15lumpsplit/QandAha.R
@@ -2,11 +2,10 @@ QandAha = function(buttonLabel= "save") {
   thisQANumber = nextNumber(sequenceType = "QA")
   outputIdThisQA = paste0('QA', thisQANumber)
   textareaIdThisQA = paste0('id', outputIdThisQA)
-  # output[[outputIdThisQA]] = renderUI({
-    textAreaInput(inputId = textareaIdThisQA, width='200%',
-                  label = paste("(QA:", thisQANumber, ")",
-                                "Ask a question and/or describe an 'aha' here.")
-    )
-  # })
-  # uiOutput(outputId = outputIdThisQA)
+  textAreaInput(inputId = textareaIdThisQA, width='200%',
+                   label =
+                         span(style='display:block; width: 200%',
+                              paste("(QA", thisQANumber, ")",
+                               "Ask a question and/or describe an 'aha' here.")
+    ) )
 }
