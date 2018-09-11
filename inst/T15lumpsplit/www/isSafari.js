9f955bb2f381da9f768e65635e3daab797a6c20c
Roger Day <notsofeo@gmail.com>
2018-09-10 21:15:10 -0400

    Adding in isFirefox detection. We should rename this file.

--- inst/T15lumpsplit/www/isSafari.js
+++ inst/T15lumpsplit/www/isSafari.js
@@ -13,6 +13,21 @@ var isSafari = navigator.vendor &&
 
 Shiny.onInputChange("isSafari", isSafari);
 
+var isFirefox =
+  navigator.userAgent &&
+  navigator.userAgent.indexOf('Mozilaa') > -1 ;
 
+Shiny.onInputChange("isSafari", isSafari);
+var isSafari = navigator.vendor &&
+  navigator.vendor.indexOf('Apple') > -1 &&
+  navigator.userAgent &&
+  navigator.userAgent.indexOf('CriOS') == -1 &&
+  navigator.userAgent.indexOf('FxiOS') == -1;
+
+Shiny.onInputChange("isSafari", isSafari);
+Shiny.onInputChange("isFirefox", isFirefox);
+
+/*
 if( ! isSafari)
  alert("Please use Safari as your browser for full features.");
+*/
