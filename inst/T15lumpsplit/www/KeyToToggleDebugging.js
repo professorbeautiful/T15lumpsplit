bc7d2a7edaabc04061d9ba58410d893f6624b008
Roger Day <notsofeo@gmail.com>
2018-09-10 21:16:41 -0400

    For Chrome but not Safari, the key handling has to be consolidated in one keyup function.

--- inst/T15lumpsplit/www/KeyToToggleDebugging.js
+++ inst/T15lumpsplit/www/KeyToToggleDebugging.js
@@ -1,15 +1,32 @@
-var ctrlDpressed = false;
+// WE MUST PUT ALL THE KEY HANDLING IN ONE PLACE
 
-$(document).on("keyup", function (e) {
-    var x = event.which || event.keyCode;
-    if(x==68 && e.ctrlKey) { // control d
+var ctrlDpressed = false;
+var keyVar = 0;
+var eventSaved = null;
+
+$(document).on("keyup", function (event) {
+    eventSaved = event;
+    x = event.which || event.keyCode;
+    keyVar = x;
+    eventSaved = event;
+    //alert('Deb ' + keyVar);
+    if(x==68 && event.ctrlKey) { // control d
       ctrlDpressed = ! ctrlDpressed;  // toggle
       Shiny.onInputChange("ctrlDpressed", ctrlDpressed);
 
       //navigateToId(currentLocationId);
       //alert('control d ' + e.ctrlKey + ' ' + event.ctrlKey);
       // The alert works.
-      // The panel appears only after something else JS happens.
+      // The panel appears only after something else JS happens.  WAIT, THAT'S NOT TRUE! IT WORKS!
       //
     }
+    Shiny.onInputChange("Latestkeypressedx", x);
+    Shiny.onInputChange("Latestkeypressede", event.code);
+    if(x==27) { // escape key
+      //navigateToId(currentLocationId);
+    Key_pressed_for_navigateToY = true;
+    Shiny.onInputChange("KeypressedfornavigateToY",
+      Key_pressed_for_navigateToY);
+    navigateToY(savedYposition);
+  }
 });
