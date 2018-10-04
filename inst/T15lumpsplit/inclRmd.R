33dd44d9b24daafb27c5ba48b2e842597129bdbd
Roger Day <notsofeo@gmail.com>
2018-10-04 13:17:13 -0400

    cosmetic change

--- inst/T15lumpsplit/inclRmd.R
+++ inst/T15lumpsplit/inclRmd.R
@@ -23,17 +23,14 @@ inclRmd <- function(path, wd, openMe=FALSE) {
     knitr::knit2html(quiet=TRUE,
                      text = ., fragment.only = TRUE, options = ""
                      #,stylesheet=file.path(r_path,"../www/empty.css"
-
-    # rmarkdown::render(quiet=TRUE,output_format = 'html_document',
+        # rmarkdown::render(quiet=TRUE,output_format = 'html_document',
                       # input = path, runtime='shiny'
     ) %>%
     gsub("&lt;!--/html_preserve--&gt;","",.) %>%
     gsub("&lt;!--html_preserve--&gt;","",.) %>%
     HTML %>%
     withMathJax %>%
-  #print(str(knitrOutput))
-  #return(knitrOutput)
-  tee(., paste0(path, '.inclOutput.Rmd'))
+    tee(., paste0(path, '.inclOutput.Rmd'))
   if(openMe)
     browseURL(paste0(path, '.inclOutput.Rmd'))
   if(!missing(wd))
