ba49816f4cff33bd84f7c07fefb464fdc1359dd5
Roger Day <notsofeo@gmail.com>
2018-10-05 21:08:40 -0400

    For running an alternative file easily. Lame but ok for now.

--- misc/rerun.R
+++ misc/rerun.R
@@ -3,11 +3,13 @@
 #   input =
 #     'inst/T15lumpsplit/Bias-variance-smoothing-shrinking.Rmd')
 # browseURL('BVout.html')
-.rerun = function(file = "inst/T15lumpsplit/Bias-variance-smoothing-shrinking.Rmd")
-  rmarkdown::run(
-    file = file,
+.rerun = function(filenum = 1) {
+  file = c("inst/T15lumpsplit/Bias-variance-smoothing-shrinking.Rmd",
+           "inst/T15lumpsplit/Bias-variance-smoothing-shrinking-testing.Rmd")[filenum]
+  rmarkdown::run(file=file,
     default_file = gsub(".*/", "", file),
     shiny_args = list(launch.browser = TRUE)
   )
+}
 
 
