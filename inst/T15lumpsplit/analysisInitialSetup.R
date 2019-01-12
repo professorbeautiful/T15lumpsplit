c6b2c81c26c48e6679c226281d3b5bc0167e501e
Roger Day <notsofeo@gmail.com>
2019-01-11 21:23:40 -0500

    Adding "cat"s to analysisInitialSetup.R  and analysisReactiveSetup.R ;   in analysisReactiveSetup.R, assign data to rValues$DLdataLastUsed

--- inst/T15lumpsplit/analysisInitialSetup.R
+++ inst/T15lumpsplit/analysisInitialSetup.R
@@ -2,9 +2,11 @@
 ##    jumpList = c(jumpList, plotLumpSplitPoints='Plot of lump & split points')
 ##  Then,   source('analysisInitialSetup.R', local=TRUE)
 
-analysisNumber = length(jumpList)
-analysisName = names(jumpList)[[analysisNumber]]
-DLdata[[analysisNumber]] = DLdataOriginal
+cat('analysisInitialSetup.R: ')
+analysisNumber <- length(jumpList)
+analysisName <- names(jumpList)[[analysisNumber]]
+cat(  analysisName, '\n')
+DLdata[[analysisNumber]] <- DLdataOriginal
 cat("Creating DLdata[[analysisNumber]] for analysisNumber = ",
     analysisNumber, '\n')
 a(name=paste0('section-a_', analysisName) )
--- inst/T15lumpsplit/analysisReactiveSetup.R
+++ inst/T15lumpsplit/analysisReactiveSetup.R
@@ -4,7 +4,9 @@
 ### First, set analysisName before
 #        source('analysisReactiveSetup.R', local=TRUE)
 
-
+cat('analysisReactiveSetup.R: ')
+cat( analysisName, '\n')
 if(is.null(rValues$DLdata[[analysisName]]))
   rValues$DLdata[[analysisName]] = DLdata[[analysisName]]
 thisData = rValues$DLdata[[analysisName]]
+rValues$DLdataLastUsed = thisData
