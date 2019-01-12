34d205dc7da3c0cbc7965f1c1d325f820ef4b55d
Roger Day <notsofeo@gmail.com>
2019-01-12 10:54:03 -0500

    more notes in analysisReactiveSetup.R

--- inst/T15lumpsplit/analysisReactiveSetup.R
+++ inst/T15lumpsplit/analysisReactiveSetup.R
@@ -2,10 +2,12 @@
 ## Initialize data for this analysis if necessary.
 
 ### First, set analysisName before
-#        source('analysisReactiveSetup.R', local=TRUE)
+#        source('analysisReactiveSetup.R', local=TRUE)        #  NO
+#        source('inst/T15lumpsplit/analysisReactiveSetup.R', local=TRUE)  # YES
+#  But this path will not work out of the installed package.
 
 cat('analysisReactiveSetup.R: ')
-cat( analysisName, '\n')
+cat( analysisName, '  ', system('pwd', intern=TRUE), '\n')
 if(is.null(rValues$DLdata[[analysisName]]))
   rValues$DLdata[[analysisName]] = DLdata[[analysisName]]
 thisData = rValues$DLdata[[analysisName]]
