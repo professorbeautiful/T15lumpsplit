fb2d4c250a3fe2166b092bbee433a9e8fbedb07d
Roger Day <notsofeo@gmail.com>
2019-01-17 16:28:02 -0500

    Just rewriting getstDLdata a bit in case debugging needed.

--- inst/T15lumpsplit/getsetDLdata.R
+++ inst/T15lumpsplit/getsetDLdata.R
@@ -1,8 +1,14 @@
-getDLdata = function(analysisName, myChoice=FALSE)
-  rValues[[ paste0(ifelse(myChoice, 'DLdataMyChoice', 'DLdata'),
-                   mapAnalysisToDTCnumber[analysisName]) ]]
+getDLdata = function(analysisName, myChoice=FALSE) {
+  index = paste0(ifelse(myChoice, 'DLdataMyChoice', 'DLdata'),
+                 mapAnalysisToDTCnumber[analysisName])
+  #cat('getDLdata: ', index, '\n')
+  rValues[[ index ]]
+}
 
-setDLdata = function(value, analysisName, myChoice=FALSE)
-  rValues[[ paste0(ifelse(myChoice, 'DLdataMyChoice', 'DLdata'),
-                   mapAnalysisToDTCnumber[analysisName]) ]] = value
+setDLdata = function(value, analysisName, myChoice=FALSE) {
+  index = paste0(ifelse(myChoice, 'DLdataMyChoice', 'DLdata'),
+         mapAnalysisToDTCnumber[analysisName])
+  #cat('setDLdata: ', index, '\n')
+  rValues[[ index ]] = value
+}
 
