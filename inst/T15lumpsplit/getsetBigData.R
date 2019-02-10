d331184034f0e4ce95e21b0b73dccb6a03e8edca
Roger Day <notsofeo@gmail.com>
2019-02-09 23:31:30 -0500

    getsetBigDate: Complication: For DTC, the data in the numericInputs and DLdata are the same numbers. For BDC, tauTrue does not determine BigData and vice versa. So we have to manage them both.

--- inst/T15lumpsplit/getsetBigData.R
+++ inst/T15lumpsplit/getsetBigData.R
@@ -1,3 +1,8 @@
+'#### Complication:
+For DTC, the data in the numericInputs and DLdata are the same numbers.
+For BDC, tauTrue does not determine BigData and vice versa.
+So we have to manage them both.'
+
 getBigData = function(analysisName, myChoice=FALSE, BDCnumber) {
   if( ! missing(analysisName))
     BDCnumber = mapAnalysisToBDCnumber[analysisName]
@@ -9,6 +14,20 @@ setBigData = function(value, analysisName, myChoice=FALSE, BDCnumber) {
   if( ! missing(analysisName))
     BDCnumber = mapAnalysisToBDCnumber[analysisName]
   index = paste0(ifelse(myChoice, 'BigDataMyChoice', 'BigData'),
+                 BDCnumber)
+  rValues[[ index ]] = value
+}
+getBigData_tauTrue = function(analysisName, myChoice=FALSE, BDCnumber) {
+  if( ! missing(analysisName))
+    BDCnumber = mapAnalysisToBDCnumber[analysisName]
+  index = paste0(ifelse(myChoice, 'BigData_tauTrue_MyChoice', 'BigData_tauTrue'),
+                 BDCnumber)
+  rValues[[ index ]]
+}
+setBigData_tauTrue = function(value, analysisName, myChoice=FALSE, BDCnumber) {
+  if( ! missing(analysisName))
+    BDCnumber = mapAnalysisToBDCnumber[analysisName]
+  index = paste0(ifelse(myChoice, 'BigData_tauTrue_MyChoice', 'BigData_tauTrue'),
                  BDCnumber)
   rValues[[ index ]] = value
 }
