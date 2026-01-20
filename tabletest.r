da94e9e1ac4b032a773af703978a350bbb756526
Roger Day <notsofeo@gmail.com>
2026-01-20 10:54:59 -0500

    Add css to an entry. Then replacing < and >. Here, making one entry red. it's goofy but it works.

--- tabletest.r
+++ tabletest.r
@@ -3,13 +3,33 @@ require('xtable')
 app =  shinyApp(
   list(ui=bootstrapPage(
          div(h1('hello'),
-             uiOutput('m')))),
+             uiOutput('m'),
+             uiOutput('m2')))),
        server = function(input,output ) {
+         wrapCSS = function(x, css) {
+           paste('<span style="', css, ';"> ', x, '</span>')
+         }
+
          output$m = renderUI({
            df <- mtcars[1:10, 1:5]
-           x = kableExtra::kbl(df)
-           print((x))
-           HTML(x)
+           df[4,3] =  wrapCSS('df43', "color:red")
+           print(df[4,3])
+           x = kableExtra::kbl(df)  %>%
+             kable_styling(full_width = F)
+           where_df43 = gregexpr('df43', x[1])[[1]]
+           cat(substr(x[1], where_df43-10, where_df43+10), '\n\n')
+           x[1] = gsub("&gt;", '>', x[1])
+           x[1] = gsub("&lt;", '<', x[1])
+           cat(substr(x[1], where_df43-10, where_df43+10), '\n\n')
+           print(x)
+           #HTML(x)  # not needed.
+         })
+         output$m2 = renderUI({
+           df <- mtcars[1:10, 1:5]
+           x = kableExtra::kbl(df) %>%
+             kable_styling(full_width = F)
+           print(x)
+           #HTML(x)  # not needed.
          })
        }
 )
