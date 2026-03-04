1b56d09336f966e99bf6d9b46d9e2d8a5e3d36b5
Roger Day <notsofeo@gmail.com>
2026-03-04 11:38:25 -0500

    Remove code to detect changed entries.  It's broken.  Also, missing "s".

--- inst/T15lumpsplit/WhoPriorProbNumericInput.R
+++ inst/T15lumpsplit/WhoPriorProbNumericInput.R
@@ -6,7 +6,7 @@ WhoPriorProbNumericInput = function() {
     numericInput(inputId = numInputIdThisWhoPriorProb,
                  label = HTML('<font color=green>Dr.Who\'s </font>prior probability that
                  <br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<font color=blue>
-                  Dr.Split\'</font> prior is correct'),
+                  Dr.Split\'s</font> prior is correct'),
                  value = 1/2, min = 0, max=1, step = 0.1)
   })
   observeEvent(input[[numInputIdThisWhoPriorProb]],
