566693fcb7360fb45a62809bafbeb4f9d9c48f30
Roger Day <notsofeo@gmail.com>
2026-08-01 10:42:20 -0400

    moving unused arrow card outside

---
+++ Episode 3 P values/arrow-fun.R
@@ -0,0 +1,11 @@
+# arrowTerminus = c(1.2, margPowerEqualLR+0.1)
+# diagram::curvedarrow(to=arrowTerminus, from=c(cutoffsSmall,
+#                                            conditionalPowerEqualLR[1]) ,
+#                      lwd = 2, lty = 1, lcol = col.v['LR'],
+#           arr.col = col.v['LR'], arr.pos = 1, curve = -.3, dr = 0.01,
+#           endhead = FALSE, segment = c(0, 1))
+# diagram::curvedarrow(to=arrowTerminus, from=c(cutoffsBig,
+#                                            conditionalPowerEqualLR[2]) ,
+#                      lwd = 2, lty = 1, lcol = col.v['LR'],
+#           arr.col = col.v['LR'], arr.pos = 1, curve = .1, dr = 0.01,
+#           endhead = FALSE, segment = c(0, 1))
